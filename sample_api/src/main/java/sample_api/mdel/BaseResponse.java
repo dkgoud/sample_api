a0344794996cece13df6cbd55dae781d2a25f74c
dkgoud <deepesh.goud@nictgroup.in>
2023-05-04 18:41:49 +0530

    first working rest api

---
+++ sample_api/src/main/java/sample_api/mdel/BaseResponse.java
@@ -0,0 +1,26 @@
+package sample_api.mdel;
+
+public class BaseResponse {
+	private String requestId;
+	private String responseCode;
+	private String responseMessage;
+	public String getRequestId() {
+		return requestId;
+	}
+	public void setRequestId(String requestId) {
+		this.requestId = requestId;
+	}
+	public String getResponseCode() {
+		return responseCode;
+	}
+	public void setResponseCode(String responseCode) {
+		this.responseCode = responseCode;
+	}
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+	
+}
---
+++ sample_api/src/main/java/sample_api/testapi.java
@@ -0,0 +1,19 @@
+package sample_api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import sample_api.mdel.BaseResponse;
+
+@RestController
+public class testapi {
+	@GetMapping("/testapi")
+	public BaseResponse test() {
+		BaseResponse res = new BaseResponse();
+		res.setRequestId("1000000");
+		res.setResponseCode("200");
+		res.setResponseMessage("OK");
+		
+		return res;
+	}
+}
