6b6ca453429fa71644e63da3983159e57bb2562a
dkgoud <deepesh.goud@nictgroup.in>
2023-05-06 18:32:03 +0530

    eureka client

--- sample_api/src/main/java/sample_api/MyApplication.java
+++ sample_api/src/main/java/sample_api/MyApplication.java
@@ -3,6 +3,7 @@ package sample_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MyApplication {
 	public static void main(String[] args) {
